e82c9396f36f2270bdaeb229122032ed58dd3d64
Adam Völgyi-Csík <vvcsadam@gmail.com>
2025-03-15 15:32:00 +0100

    Add Domain layer with entities and enums

---
+++ VatCalculatorApi/Domain/Entities/VatCalculation.cs
@@ -0,0 +1,12 @@
+﻿using VatCalculatorApi.Domain.Enums;
+
+namespace VatCalculatorApi.Domain.Entities
+{
+    public class VatCalculation
+    {
+        public decimal Net { get; set; }
+        public decimal Gross { get; set; }
+        public decimal Vat { get; set; }
+        public VatRate VatRate { get; set; }
+    }
+}
---
+++ VatCalculatorApi/Domain/Enums/VatRate.cs
@@ -0,0 +1,9 @@
+﻿namespace VatCalculatorApi.Domain.Enums
+{
+    public enum VatRate
+    {
+        Vat10 = 10,
+        Vat20 = 20,
+        Vat30 = 30
+    }
+}
