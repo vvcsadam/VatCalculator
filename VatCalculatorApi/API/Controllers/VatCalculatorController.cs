c7536771133b6d137b5d39737eefa9a625bb2214
Adam Völgyi-Csík <vvcsadam@gmail.com>
2025-03-15 15:17:59 +0100

    Init

---
+++ VatCalculatorApi/API/Controllers/VatCalculatorController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VatCalculatorApi.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VatCalculatorController : ControllerBase
+    {
+    }
+}
